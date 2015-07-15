2710daa5452da2e08a0c6e293adcdedc109d3401
WZ <lwz812@gmail.com>
2015-07-15 16:55:28 -0700

    boost jackson version

--- org.cg.services.core/src/main/java/org/cg/services/core/util/CGDateTimeSerializer.java
+++ org.cg.services.core/src/main/java/org/cg/services/core/util/CGDateTimeSerializer.java
@@ -2,10 +2,10 @@ package org.cg.services.core.util;
 
 import java.io.IOException;
 
-import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.map.JsonSerializer;
-import org.codehaus.jackson.map.SerializerProvider;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 /**
  * A JsonSerializer that serializes {@link CGDateTime}
