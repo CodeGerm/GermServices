2cc1d978f38b4cf2f5ed38f6c5c5240bbbe4a1b5
Yanlin Wang <yanlin.wang@centrify.com>
2016-02-17 22:33:11 -0800

    refactor ping ws

--- org.cg.services.core/src/main/java/org/cg/services/core/websocket/controller/PingController.java
+++ org.cg.services.core/src/main/java/org/cg/services/core/websocket/controller/PingController.java
@@ -3,6 +3,7 @@ package org.cg.services.core.websocket.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -15,6 +16,7 @@ public class PingController {
 	private static final Log LOG = LogFactory.getLog(PingController.class);
 
 	@Autowired
+	@Qualifier("org.springframework.messaging.simp.SimpMessagingTemplate#1")
 	public SimpMessageSendingOperations messagingTemplate;
 
 	@MessageMapping("/ping")
