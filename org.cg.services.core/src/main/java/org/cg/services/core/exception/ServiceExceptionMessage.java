aa48c49617ac141f10a2a500d089c72bebc2fc79
Weizhi Li <lwz812@gmail.com>
2016-02-25 00:04:27 -0800

    service error message to json

--- org.cg.services.core/src/main/java/org/cg/services/core/exception/ServiceExceptionMessage.java
+++ org.cg.services.core/src/main/java/org/cg/services/core/exception/ServiceExceptionMessage.java
@@ -1,5 +1,9 @@
 package org.cg.services.core.exception;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * A exception message that can be returned in error response
  * @author WZ
@@ -10,6 +14,9 @@ public class ServiceExceptionMessage {
 	private final int errorCode;
 	private final String errorType;
 	private final String error;
+	private final static Gson gson = new GsonBuilder()
+									.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+									.create();
 	
 	public ServiceExceptionMessage(int errorCode, String errorType, String error) {
 		super();
@@ -30,4 +37,7 @@ public class ServiceExceptionMessage {
 		return error;
 	}
 
+	public String toJson() {
+		return gson.toJson(this);
+	}
 }
