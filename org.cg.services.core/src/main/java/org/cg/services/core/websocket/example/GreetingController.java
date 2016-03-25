8ef04dfaa033aa713f07738852467451c93bc15a
Weizhi Li <lwz812@gmail.com>
2016-03-24 22:32:53 -0700

    merge ws example and add scheme setting for swagger

--- org.cg.services.core/src/main/java/org/cg/services/core/websocket/example/GreetingController.java
+++ org.cg.services.core/src/main/java/org/cg/services/core/websocket/example/GreetingController.java
@@ -2,8 +2,9 @@ package org.cg.services.core.websocket.example;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -16,11 +17,14 @@ public class GreetingController {
 
 	private static final Log LOG = LogFactory.getLog(GreetingController.class);
 
+	@Autowired
+	public SimpMessageSendingOperations messagingTemplate;
+	
 	@MessageMapping("/hello")
-	@SendTo("/cg/websockets/topic/greetings")
-	public Greeting greeting(HelloMessage message) throws Exception {
-		LOG.info(String.format("Processing message: %s", message.getName()));
-		Thread.sleep(3000); // simulated delay
-		return new Greeting("Hello, " + message.getName() + "!");
+	public void greeting(HelloMessage message) throws Exception {
+		LOG.info("Ping client");
+		Thread.sleep(1000); // simulated delay
+		messagingTemplate.convertAndSend("/cg/websockets/topic/greetings", 
+				new Greeting("Hello, " + message.getName() + "!"));
 	}
 }	
